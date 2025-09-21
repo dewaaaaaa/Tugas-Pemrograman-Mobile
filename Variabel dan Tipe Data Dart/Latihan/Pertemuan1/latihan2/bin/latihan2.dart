f0b345c60470b5e3d3544cbc82c10be6fdc5b1bb
Dewa Maulana <mdewa057@gmail.com>
2025-09-21 22:44:28 +0900

    Penambahan Tugas1 dan Latihan2

---
+++ Variabel dan Tipe Data Dart/Latihan/Pertemuan1/latihan2/bin/latihan2.dart
@@ -0,0 +1,15 @@
+import 'package:latihan2/latihan2.dart' as latihan2;
+
+void main(List<String> arguments) {
+  //deklarasi variabel yang dibutuhkan
+  double panjang = 10;
+  double lebar = 5;
+  double luas = panjang * lebar;
+  double keliling = 2 * (panjang + lebar);
+
+  //menampilkan output
+  print("Panjang  = $panjang");
+  print("Lebar    = $lebar");
+  print("Luas     = $luas");
+  print("Keliling = $keliling");
+}
